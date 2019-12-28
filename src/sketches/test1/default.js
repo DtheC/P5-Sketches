87fccf62b13364a4bf7ea0816b018f88a7872762
Travis <travis@denouncetheclock.com>
2019-12-28 13:10:22 +1100

    Fractal iteration

--- src/sketches/test1/default.js
+++ src/sketches/test1/default.js
@@ -2,20 +2,21 @@ const CELL_WIDTH = 20;
 const CELL_HEIGHT = 20;
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 600;
-const ITERATIONS = 2;
+const ITERATIONS = 3;
 
 function setup() {
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   this.cells = [];
-  // for (let y = 0; y < CANVAS_HEIGHT; y += CELL_HEIGHT) {
-  //   for (let x = 0; x < CANVAS_WIDTH; x += CELL_WIDTH) {
-  //     cells.push(new Cell(createVector(x, y, 0), createVector(20, 20, 0)));
-  //   }
-  // }
-  const c = new Cell(createVector(0, 0, 0), createVector(CANVAS_WIDTH, CANVAS_HEIGHT, 0));
-  this.cells = iterate(c);
+  this.cells.push(new Cell(createVector(0, 0, 0), createVector(CANVAS_WIDTH, CANVAS_HEIGHT, 0)));
 
-
+  for (let index = 0; index < ITERATIONS; index++) {
+    const oldCells = this.cells.slice(0);
+    this.cells = [];
+    oldCells.forEach((elem) => {
+      const newCells = iterate(elem);
+      newCells.forEach(newElem => this.cells.push(newElem));
+    });
+  }
 
   noStroke();
   this.cells.forEach(element => {
@@ -37,22 +38,6 @@ function iterate(cellToIterate) {
   }
   if (cells.length == 0) cells.push(cellToIterate);
   return cells;
-}
-
-function draw() {
-  // noStroke();
-  // this.cells.forEach(element => {
-  //   element.draw();
-  // });
-  // if (mouseIsPressed) {
-  //   fill(0);
-  //   stroke(255);
-  // } else {
-  //   fill(255);
-  //   stroke(0);
-  // }
-  // ellipse(mouseX, mouseY, 80, 80);
-
 }
 
 class Cell {
