9d7f92e637c1d2a566115af10965b254f83fed1c
Travis <travis@denouncetheclock.com>
2019-12-29 08:36:20 +1100

    Added redraws on iteration to animate generation

---
+++ src/sketches/cell-iteration-3/default.js
@@ -0,0 +1,61 @@
+const CELL_WIDTH = 20;
+const CELL_HEIGHT = 20;
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 600;
+const ITERATIONS = 5;
+
+function setup() {
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  this.colourScheme = ColorSchemeController.getSchemeWithXColours(5);
+  this.cells = [];
+  this.cells.push(new Cell(createVector(0, 0, 0), createVector(CANVAS_WIDTH, CANVAS_HEIGHT, 0), color(this.colourScheme[0])));
+
+  this.generator = iterateCells();
+  this.oldStamp = 0;
+  requestAnimationFrame(runGen);
+}
+
+function runGen(timeStamp) {
+  if (timeStamp - this.oldStamp > 100) {
+    this.generator.next();
+    this.oldStamp = timeStamp;
+  }
+  requestAnimationFrame(runGen);
+}
+
+function drawCell(cellArray) {
+  noStroke();
+  cellArray.forEach(e => e.draw());
+}
+
+function * iterateCells() {
+  for (let index = 1; index < ITERATIONS; index++) {
+    console.log(`Iteration ${index} in progress...`);
+    const oldCells = this.cells.slice(0);
+    this.cells = [];
+    for (let oldCellIndex = 0; oldCellIndex < oldCells.length; oldCellIndex++) {
+      const newCells = iterate(oldCells[oldCellIndex], this.colourScheme[index-1]);
+      for (let newCellIndex = 0; newCellIndex < newCells.length; newCellIndex++) {
+          drawCell([newCells[newCellIndex]]);
+          this.cells.push(newCells[newCellIndex]);
+      }
+      yield null;
+    }
+  }
+}
+
+function iterate(cellToIterate, nextColour) {
+  const cells = [];
+  const split = (Math.round(Math.random() * 5)) * 2; // Even numbers only
+  const newCellWidth = cellToIterate.size.x / split;
+  const newCellHeight = cellToIterate.size.y / split;
+  const newCellSize = createVector(newCellWidth, newCellHeight, 0);
+  for (let y = 0; y < split; y++) {
+    for (let x = 0; x < split; x++) {
+      const loc = createVector(cellToIterate.position.x + (x * newCellWidth), cellToIterate.position.y + (y * newCellHeight), 0);
+      cells.push(new Cell(loc, newCellSize, nextColour));
+    }
+  }
+  if (cells.length == 0) cells.push(cellToIterate);
+  return cells;
+}
--- src/sketches/shared/colorSchemes.js
+++ src/sketches/shared/colorSchemes.js
@@ -2,9 +2,9 @@ class ColourSchemes {
   constructor() {
     // TODO: would be good to load them from a json file but this works for now
     this._schemes = [
-      // ['#e27d5f', '#85cdca', '#e8a87c', '#c38d9d', '#40b3a2'],
-      // ['#8e8741', '#659ebc', '#daad86', '#bd986b', '#fbeec1'],
-      // ['#fc4444', '#3feee7', '#55bdca', '#96caef', '#cafafe'],
+      ['#e27d5f', '#85cdca', '#e8a87c', '#c38d9d', '#40b3a2'],
+      ['#8e8741', '#659ebc', '#daad86', '#bd986b', '#fbeec1'],
+      ['#fc4444', '#3feee7', '#55bdca', '#96caef', '#cafafe'],
       ['#1a1a1c', '#4e4e50', '#6f2232', '#950741', '#c3083f']
     ]
   }
