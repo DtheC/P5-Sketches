8c4c491e3574b82f8893f31ec23416970ab59a2d
Travis <travis@denouncetheclock.com>
2020-01-04 17:17:58 +1100

    Add 9 and 10

--- src/sketches/shared/cell.js
+++ src/sketches/shared/cell.js
@@ -3,6 +3,11 @@ class Cell {
     this.position = coordinates;
     this.size = size;
     this.colour = colour ? colour : color(Math.random() * 255);
+    this.initVariables();
+  }
+
+  initVariables() {
+    // Nothing
   }
 
   draw() {
@@ -15,5 +20,27 @@ class CellRound extends Cell {
   draw() {
     fill(this.colour);
     ellipse(this.position.x + (this.size.x / 2), this.position.y + (this.size.y / 2), this.size.x, this.size.y);
+  }
+}
+
+class CellOutline extends Cell {
+  initVariables() {
+    this.rotation = 0;
+  }
+
+  draw() {
+    if (this.size.x > 4) {
+      fill(0, 0);
+      stroke(this.colour);
+      let strokeSize = this.size.x / 10;
+      if (strokeSize < 1) strokeSize = 1;
+      strokeWeight(strokeSize);
+      push();
+      translate(this.position.x + (this.size.x / 2), this.position.y + (this.size.y / 2));
+      rotate(this.rotation);
+      rect(-this.size.x / 2, -this.size.y / 2, this.size.x, this.size.y);
+      line(-this.size.x / 2, -this.size.y / 2, this.size.x / 2, this.size.y / 2);
+      pop();
+    }
   }
 }
